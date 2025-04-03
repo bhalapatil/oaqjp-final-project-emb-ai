e5271ca2b025126dc6c6e79e1615ab371080fe65
bhalapatil <bhalapatil1@gmail.com>
2025-04-03 18:49:33 +0530

    Error handling

--- server.py
+++ server.py
@@ -6,8 +6,10 @@ app = Flask("Emotion Detector")
 @app.route('/emotionDetector')
 def detect_emotion():
     text_to_analyze = request.args.get("textToAnalyze")
-    emotion_dict = emotion_detector(text_to_analyze)
-    stmt = f"For the given statement, the system response is 'anger': {emotion_dict['anger']} , 'disgust' : {emotion_dict['disgust']} , 'fear' : {emotion_dict['fear']}, 'joy' : {emotion_dict['joy']} and 'sadness' : {emotion_dict['sadness']}. The dominant emtion is {emotion_dict['dominant_emotion']}"
-    return stmt
-
-
+    if text_to_analyze:
+        emotion_dict = emotion_detector(text_to_analyze)
+        if not emotion_detection['dominant_emotion']:
+            return "Invalid text! Please try again!."
+        else:    
+            stmt = f"For the given statement, the system response is 'anger': {emotion_dict['anger']} , 'disgust' : {emotion_dict['disgust']} , 'fear' : {emotion_dict['fear']}, 'joy' : {emotion_dict['joy']} and 'sadness' : {emotion_dict['sadness']}. The dominant emtion is {emotion_dict['dominant_emotion']}"
+            return stmt
